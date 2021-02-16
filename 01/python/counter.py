80d5b5691a073e8b348b9123ae98b795e9b70046
Patrik Vavercak <patrik.vavercak@innovatrics.com>
2021-02-16 22:49:56 +0100

    feat: add python program with 3 different counter functions

---
+++ 01/python/counter.py
@@ -0,0 +1,62 @@
+from fei.ppds import Thread, Mutex
+
+
+class Shared():
+    def __init__(self, end):
+        self.counter = 0
+        self.end = end
+        self.array = [0] * self.end
+        self.mutex = Mutex()
+
+    def safe_increment(self):
+        if self.counter < self.end:
+            self.array[self.counter] += 1
+
+
+class Histogram(dict):
+    def __init__(self, seq=[]):
+        for item in seq:
+            self[item] = self.get(item, 0) + 1
+
+
+def counter1(shared):
+    while True:
+        if shared.counter >= shared.end:
+            break
+        shared.mutex.lock()
+        shared.array[shared.counter] += 1
+        shared.counter += 1
+        shared.mutex.unlock()
+
+
+def counter2(shared):
+    while True:
+        shared.mutex.lock()
+        if shared.counter >= shared.end:
+            shared.mutex.unlock()
+            break
+        shared.array[shared.counter] += 1
+        shared.counter += 1
+        shared.mutex.unlock()
+
+
+def counter3(shared):
+    while True:
+        shared.mutex.lock()
+        cnt = shared.counter
+        shared.counter +=1
+        shared.mutex.unlock()
+        if cnt >= shared.end:
+            break
+        shared.array[cnt] += 1
+
+
+for i in range(5):
+    sh = Shared(1_000_000)
+    t1 = Thread(counter3, sh)
+    t2 = Thread(counter3, sh)
+
+    t1.join()
+    t2.join()
+
+    print(Histogram(sh.array))
