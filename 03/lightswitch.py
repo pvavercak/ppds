ee518dd38744a309b3d31b713d02656f7b1d28d2
Patrik Vavercak <patrik.vavercak@innovatrics.com>
2021-03-03 22:04:30 +0100

    feat: add lightswitch implementation

--- 03/lightswitch.py
+++ 03/lightswitch.py
@@ -3,10 +3,19 @@ from fei.ppds import Thread, Mutex, Semaphore
 
 class LightSwitch:
     def __init__(self):
-        pass
+        self.cnt = 0
+        self.mutex = Mutex()
 
     def lock(self, sem):
-        pass
+        self.mutex.lock()
+        self.cnt += 1
+        if self.cnt == 1:
+            sem.wait()
+        self.mutex.unlock()
 
     def unlock(self, sem):
-        pass
+        self.mutex.lock()
+        self.cnt -= 1
+        if self.cnt == 0:
+            sem.signal()
+        self.mutex.unlock()
