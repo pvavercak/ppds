dd5c4431918104ddc6410e4a1661e3e95ce03ef5
Patrik Vavercak <patrik.vavercak@innovatrics.com>
2021-03-03 23:39:17 +0100

    feat: use producer-consumer in function

--- 03/producer_consumer.py
+++ 03/producer_consumer.py
@@ -1,4 +1,4 @@
-from fei.ppds import Mutex, Semaphore
+from fei.ppds import Mutex, Semaphore, Thread
 from time import sleep
 from random import randint
 
@@ -26,3 +26,18 @@ def consumer(warehouse, time_to_gain):
     warehouse.mutex.unlock()
     warehouse.free_space.signal()
     sleep(randint(1, 10) / 500)
+
+
+def producer_consumer_benchmark():
+    warehouse = WareHouse(10)
+    consumers = [Thread(consumer, warehouse, 0.05) for _ in range(10)]
+    producers = [
+        Thread(producer, warehouse, 0.05, 0.05) for _ in range(10)
+    ]
+
+    wait = 0.5
+    sleep(wait)
+    warehouse.items.signal(100)
+    warehouse.free_space.signal(100)
+    for thread in consumers + producers:
+        thread.join()
