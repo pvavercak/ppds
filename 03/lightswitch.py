ca253f94a34d7246005fe18209386f6ab06151fc
Patrik Vavercak <patrik.vavercak@innovatrics.com>
2021-03-03 22:00:02 +0100

    feat: add basic lightswitch structure

---
+++ 03/lightswitch.py
@@ -0,0 +1,12 @@
+from fei.ppds import Thread, Mutex, Semaphore
+
+
+class LightSwitch:
+    def __init__(self):
+        pass
+
+    def lock(self, sem):
+        pass
+
+    def unlock(self, sem):
+        pass
