630064b3dfee6abc4e78cbd9ae8a36ed4deb4afb
Patrik Vavercak <patrik.vavercak@innovatrics.com>
2021-03-03 23:28:56 +0100

    feat: add full consumer implementation

--- 03/producer_consumer.py
+++ 03/producer_consumer.py
@@ -1,5 +1,6 @@
 from fei.ppds import Mutex, Semaphore
 from time import sleep
+from random import randint
 
 
 class WareHouse:
@@ -18,5 +19,10 @@ def producer(warehouse, time_to_produce, time_to_store):
     warehouse.items.signal()
 
 
-def consumer(warehouse):
-    pass
+def consumer(warehouse, time_to_gain):
+    warehouse.items.wait()
+    warehouse.mutex.lock()
+    sleep(time_to_gain)
+    warehouse.mutex.unlock()
+    warehouse.free_space.signal()
+    sleep(randint(1, 10) / 500)
