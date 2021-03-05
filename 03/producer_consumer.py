83ee249d43b699466b642a2f684df8b764cb3244
Patrik Vavercak <patrik.vavercak@innovatrics.com>
2021-03-05 21:19:42 +0100

    feat: improve benchmark function

--- 03/producer_consumer.py
+++ 03/producer_consumer.py
@@ -17,6 +17,12 @@ class WareHouse:
         self.produced += 1
         self.mutex.unlock()
 
+    def stop_production(self, time_period):
+        sleep(time_period)
+        self.closed = True
+        self.items.signal(100)
+        self.free_space.signal(100)
+
 
 def producer(warehouse, time_to_produce, time_to_store):
     while True:
@@ -42,34 +48,24 @@ def consumer(warehouse, time_to_gain):
             break
 
 
-def producer_consumer_benchmark():
+def producer_consumer_benchmark(repetitions, service_time):
     keys = ["production_times", "producers_count", "produced_items"]
     data = {keys[0]: [], keys[1]: [], keys[2]: []}
-    repeat = 1
-    service_time = 0.1
     for t_production in range(1, 5):
         for n_producers in range(1, 5):
             partial_sum = 0
             production_time = t_production / 100
-            for _ in range(repeat):
+            for _ in range(repetitions):
                 warehouse = WareHouse(10)
                 consumers = [Thread(consumer, warehouse, 0.05)
                              for _ in range(10)]
                 producers = [
                     Thread(producer, warehouse, production_time, 0.01)
                     for _ in range(n_producers)]
-
-                sleep(service_time)
-                warehouse.closed = True
-                warehouse.items.signal(100)
-                warehouse.free_space.signal(100)
-
-                for thread in consumers + producers:
-                    thread.join()
-
+                warehouse.stop_production(service_time)
+                [thread.join() for thread in consumers + producers]
                 partial_sum += warehouse.produced / service_time
-            produced_per_sec = partial_sum / repeat
-            print(produced_per_sec)
+            produced_per_sec = partial_sum / repetitions
             data[keys[0]].append(production_time)
             data[keys[1]].append(n_producers)
             data[keys[2]].append(produced_per_sec)
