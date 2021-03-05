f215f2aeaf45a20b57653a41ae815d118d59bac3
Patrik Vavercak <patrik.vavercak@innovatrics.com>
2021-03-05 21:02:50 +0100

    feat: finish benchmark function

--- 03/producer_consumer.py
+++ 03/producer_consumer.py
@@ -38,16 +38,34 @@ def consumer(warehouse, time_to_gain):
 
 
 def producer_consumer_benchmark():
-    warehouse = WareHouse(10)
-    consumers = [Thread(consumer, warehouse, 0.05) for _ in range(10)]
-    producers = [
-        Thread(producer, warehouse, 0.05, 0.05) for _ in range(10)
-    ]
+    keys = ["production_times", "producers_count", "produced_items"]
+    data = {keys[0]: [], keys[1]: [], keys[2]: []}
+    repeat = 1
+    service_time = 0.1
+    for t_production in range(1, 5):
+        for n_producers in range(1, 5):
+            partial_sum = 0
+            production_time = t_production / 100
+            for _ in range(repeat):
+                warehouse = WareHouse(10)
+                consumers = [Thread(consumer, warehouse, 0.05)
+                             for _ in range(10)]
+                producers = [
+                    Thread(producer, warehouse, production_time, 0.01)
+                    for _ in range(n_producers)]
 
-    wait = 0.5
-    sleep(wait)
-    warehouse.closed = True
-    warehouse.items.signal(100)
-    warehouse.free_space.signal(100)
-    for thread in consumers + producers:
-        thread.join()
+                sleep(service_time)
+                warehouse.closed = True
+                warehouse.items.signal(100)
+                warehouse.free_space.signal(100)
+
+                for thread in consumers + producers:
+                    thread.join()
+
+                partial_sum += warehouse.produced / service_time
+            produced_per_sec = partial_sum / repeat
+            print(produced_per_sec)
+            data[keys[0]].append(production_time)
+            data[keys[1]].append(n_producers)
+            data[keys[2]].append(produced_per_sec)
+    return data
