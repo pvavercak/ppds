5e108ca48f13ba7589c3bd0a6f17e0a093150f9b
Patrik Vavercak <patrik.vavercak@innovatrics.com>
2021-04-06 23:23:17 +0200

    feat: create basic structure for number multiplicator script

---
+++ 08/multiply.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def dispatch(multipliers):
+    try:
+        pass
+    except GeneratorExit:
+        pass
+
+
+def send_numbers(nums, dispatch_fnc):
+    pass
+
+
+def num_multiplier(multiplier, printer):
+    try:
+        pass
+    except GeneratorExit:
+        pass
+
+
+def num_printer(multiplier):
+    try:
+        pass
+    except GeneratorExit:
+        pass
