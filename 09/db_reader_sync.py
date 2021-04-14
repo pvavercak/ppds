e7ec092aefeb88d6e7c7eada166e065d05211208
Patrik Vavercak <patrik.vavercak@innovatrics.com>
2021-04-14 19:32:40 +0200

    feat: change get_capital to generator function

--- 09/db_reader_sync.py
+++ 09/db_reader_sync.py
@@ -20,6 +20,7 @@ def get_capital(task_name, work_queue, db_connection):
         result = db_connection.execute(query)
         row = result.fetchone()
         print(f"{task_name}: {country} - {row[0]}")
+        yield
 
 
 def main():
@@ -28,7 +29,19 @@ def main():
         countries.put(country)
 
     with sqlite3.connect("capitals.db") as db_connection:
-        get_capital("One", countries, db_connection)
+        tasks = [
+            get_capital("One", countries, db_connection),
+            get_capital("Two", countries, db_connection)]
+
+        done = False
+        while not done:
+            for task in tasks:
+                try:
+                    next(task)
+                except StopIteration:
+                    tasks.remove(task)
+                if len(tasks) == 0:
+                    done = True
 
 
 if __name__ == "__main__":
