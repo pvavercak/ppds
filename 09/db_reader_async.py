c4b58979bafe878f90ceb71beece4ebd68eee5cb
Patrik Vavercak <patrik.vavercak@innovatrics.com>
2021-04-14 19:39:23 +0200

    feat: use native coroutines in async version of db_reader

--- 09/db_reader_async.py
+++ 09/db_reader_async.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import sqlite3
-import queue
+import aiosqlite
+import asyncio
 import time
 
 COUNTRIES = [
@@ -15,51 +15,38 @@ COUNTRIES = [
 sum_times = 0
 
 
-def get_capital(task_name, work_queue, db_connection):
+async def get_capital(task_name, work_queue, db_connection):
     global sum_times
     while not work_queue.empty():
-        country = work_queue.get()
+        country = await work_queue.get()
         query = f"SELECT capital FROM CAPITALS WHERE country='{country}'"
         print(f"{task_name}: running")
         t = time.perf_counter_ns()
 
-        result = db_connection.execute(query)
-        row = result.fetchone()
-
-        diff = time.perf_counter_ns() - t
-        print(f"{task_name}: {country} - {row[0]},"
-              f" time elapsed {diff} [ns]")
-        sum_times += diff
-        yield
+        async with db_connection.execute(query) as result:
+            row = await result.fetchone()
+            diff = time.perf_counter_ns() - t
+            print(f"{task_name}: {country} - {row[0]},"
+                  f" time elapsed {diff} [ns]")
+            sum_times += diff
 
 
-def main():
-    countries = queue.Queue()
+async def main():
+    countries = asyncio.Queue()
     for country in COUNTRIES:
-        countries.put(country)
+        await countries.put(country)
 
-    with sqlite3.connect("capitals.db") as db_connection:
+    async with aiosqlite.connect("capitals.db") as db_connection:
         tasks = [
             get_capital("One", countries, db_connection),
             get_capital("Two", countries, db_connection)]
 
-        done = False
         t = time.perf_counter_ns()
-        diff = None
-
-        while not done:
-            for task in tasks:
-                try:
-                    next(task)
-                except StopIteration:
-                    tasks.remove(task)
-                if len(tasks) == 0:
-                    diff = time.perf_counter_ns() - t
-                    done = True
-
+        await asyncio.gather(*tasks)
+        diff = time.perf_counter_ns() - t
         print(f"{diff} [ns] vs {sum_times} [ns]"
               " (app runtime vs sum of partial times)")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
